f7631265721d4a9c6e1c593a4f2e216323d9f79a
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-06-29 23:18:05 -0300

    adicionada nova rota

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,10 +1,12 @@
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from "@angular/router";
-import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriasComponent } from './pasta-categorias/categorias/categorias.component';
 import { ModuleWithProviders } from '@angular/core';
+import { DetalheCategoriaComponent } from './pasta-categorias/detalhe-categoria/detalhe-categoria.component';
 
 const APP_ROUTES: Routes = [
     { path: 'categorias', component: CategoriasComponent},
+    { path: 'categorias/:id', component: DetalheCategoriaComponent},
     { path: '', component: HomeComponent}
 ];
 
