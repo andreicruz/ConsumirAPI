fda11da4714feef467eaee65e659525f7e13ef36
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-06-30 23:56:43 -0300

    formulario de criacao

--- src/app/pasta-categorias/criar-categoria/criar-categoria.component.ts
+++ src/app/pasta-categorias/criar-categoria/criar-categoria.component.ts
@@ -1,4 +1,8 @@
+import { CategoriasServicoService } from './../categorias-servico.service';
+import { Categoria } from './../categoria';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-categoria',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarCategoriaComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria;
+  form: FormGroup;
+
+  constructor(private categoriaServico: CategoriasServicoService, private fb: FormBuilder, private route: Router) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(200)]],
+      descricao: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(200)]]
+    });
   }
 
+  addCategoria(){
+    console.log(this.form.value);
+    if (this.form.valid) {
+      console.log('submit');
+      this.categoriaServico.addCategoria(this.form.value).subscribe(() => this.route.navigateByUrl('/categorias'));
+      // this.categoriaServico.updateCategoria(this.categoria).subscribe(() => this.categoriaServico.backPage());
+    }
+   // this.categoriaServico.addCategoria(this.categoria).subscribe(categoria => this.categoria.push(categoria))
+  }
+
+  limpar() {
+    this.form.reset();
+  }
 }
