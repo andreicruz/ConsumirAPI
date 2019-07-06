25f7d7eaef440ccd646eff47fca7a1b6cf553bf6
andreicruz <andrei_cruz97@hotmail.com>
2019-07-06 20:38:48 -0300

    alteracao layout do form

--- src/app/pasta-categorias/detalhe-categoria/detalhe-categoria.component.ts
+++ src/app/pasta-categorias/detalhe-categoria/detalhe-categoria.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute } from '@angular/router';
 import { CategoriasServicoService } from '../categorias-servico.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-detalhe-categoria',
@@ -10,8 +11,11 @@ import { Component, OnInit } from '@angular/core';
 export class DetalheCategoriaComponent implements OnInit {
 
   categoria: any;
+  form: FormGroup;
+  nome = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  descricao = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  constructor(private categoriaServico: CategoriasServicoService, private route: ActivatedRoute) { }
+  constructor(private categoriaServico: CategoriasServicoService, private route: ActivatedRoute, private fb: FormBuilder,) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -20,9 +24,31 @@ export class DetalheCategoriaComponent implements OnInit {
         this.categoria = categoria;
       })
     })
+    this.form = this.fb.group({
+      id: [],
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      descricao: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]]
+    });
+  }
+
+  getErrorMessageNome(){
+    return this.nome.hasError('required') ? 'Mínimo 3 digitos' :
+    this.nome.hasError('nome') ? 'Nome inválido' : '';
+  }
+
+  getErrorMessageDescricao(){
+    return this.descricao.hasError('required') ? 'Mínimo 3 digitos' : 
+    this.descricao.hasError('descricao') ? 'Descrição inválida' : '';
   }
 
   updateCategoria(){
-    this.categoriaServico.updateCategoria(this.categoria).subscribe(() => this.categoriaServico.backPage());
+    if (this.form.valid) {
+      console.log('submited');
+      this.categoriaServico.updateCategoria(this.form.value).subscribe(() => this.categoriaServico.backPage());
+    }
+  }
+
+  limpar() {
+    this.form.reset();
   }
 }
